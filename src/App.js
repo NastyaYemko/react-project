11a005379c7681594cab4f6c5a499ac9c592bc10
Nastya Yemko <nastya.yemko@gmail.com>
2021-09-19 17:58:30 +0300

    create styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {useState} from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import PostItem from "./components/PostItem";
+import './styles/App.css'
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
-      <ClassCounter/>
+      <PostItem/>
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PostItem = () => {
+    return (
+        <div className="post">
+            <div className="post__content">
+                <strong>1. JavaScript</strong>
+                <div>JavaScript - programming language</div>
+            </div>
+            <div className="post__btns">
+                <button>Delete</button>
+            </div>
+        </div>
+    );
+};
+
+export default PostItem;
