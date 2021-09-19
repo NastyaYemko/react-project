3312d2e1f76cca3ba229398ee30f9c34de04c0b6
Nastya Yemko <nastya.yemko@gmail.com>
2021-09-19 16:33:31 +0300

    create first function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  const [likes, setLikes] = useState(5) // значение по умолчанию 5
+
+  function increment() {
+    setLikes(likes + 1)
+  }
+
+  function decrement() {
+    setLikes(likes - 1)
+  }
+
   return (
     <div className="App">
-
+      <h1>{likes}</h1>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
     </div>
   );
 }
