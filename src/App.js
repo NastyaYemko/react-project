d012b8a0c44b6ae0568f5c291e60da6351980acc
Nastya Yemko <nastya.yemko@gmail.com>
2021-09-19 16:59:29 +0300

    subscribe to the event

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 
 function App() {
   const [likes, setLikes] = useState(5) // значение по умолчанию 5
+  const [value, setValue] = useState('Some value')
 
   function increment() {
     setLikes(likes + 1)
@@ -14,6 +15,12 @@ function App() {
   return (
     <div className="App">
       <h1>{likes}</h1>
+      <h1>{value}</h1>
+        <input
+            type="text"
+            value={value}
+            onChange={event => setValue(event.target.value)}
+        />
       <button onClick={increment}>increment</button>
       <button onClick={decrement}>decrement</button>
     </div>
