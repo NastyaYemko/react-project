3878ce49dbd8966b3b6818cdf573f41ccd36a462
Nastya Yemko <nastya.yemko@gmail.com>
2021-09-19 18:34:25 +0300

    props

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import PostItem from "./components/PostItem";
 import './styles/App.css'
+import PostList from "./components/PostList";
 
 function App() {
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JavaScript 1', body: 'Description'},
+        {id: 2, title: 'JavaScript 2', body: 'Description'},
+        {id: 3, title: 'JavaScript 3', body: 'Description'},
+    ])
   return (
     <div className="App">
-      <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}}/>
+      <PostList posts={posts}/>
     </div>
   );
 }
